d56ed554c586f6e5cf6075f56eb78c4a2d77ee53
richardmoyles <Richardmoyles@gmail.com>
2014-09-26 15:43:38 +0100

    Converted the file to a bash script executable

--- cpucount.sh
+++ cpucount.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+# Title : cpucount.sh
+# Date : 26/09/2014
+# Author : Richard Moyles
+# Version :1.0
+# Description : Counts the CPU's 
+# Options : 
+
 grep processor /proc/cpuinfo| wc -l
